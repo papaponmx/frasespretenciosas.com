0920432762328075004488c6228cebc2983edace
papaponmx <jaimemadrigalrios@gmail.com>
2019-01-07 22:22:42 -0600

    First commit

---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `Frases Pretenciosas`,
+  siteTitleShort: `frases pretenciosas.com`,
+  siteDescription: `Obten las frases pretenciosas que todo milenial, influencer necesita en sus fotos`,
+  siteUrl: `https://frasespretenciosas.com`,
+  themeColor: `#000`,
+  backgroundColor: `#fff`,
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: `gatsbyjs`,
+    fbAppId: `966242223397117`,
+  },
+};
