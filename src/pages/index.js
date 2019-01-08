a19aecb45f190f7a2008889ca682e0a7aaf2279c
papaponmx <jaimemadrigalrios@gmail.com>
2019-01-07 23:14:53 -0600

    Removed IO example

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,6 @@ import PropTypes from 'prop-types';
 import Layout from 'components/layout';
 import Box from 'components/box';
 import Title from 'components/title';
-import Gallery from 'components/gallery';
-import IOExample from 'components/io-example';
-import Modal from 'containers/modal';
 import { graphql } from 'gatsby';
 
 const Index = ({ data }) => (
@@ -16,7 +13,6 @@ const Index = ({ data }) => (
       </Title>
     </Box>
     <div style={{ height: '50vh' }} />
-    <IOExample />
   </Layout>
 );
 
